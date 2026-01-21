a36cf9ca5f5695ba04e9f283a502bfbe9003a5c8
RiDDiX <github@egu-rel.de>
2026-01-21 23:04:53 +0100

    fix: version extraction with grep, bump to 2.3.1, add manifest output for debugging

--- page/plugin.config.js
+++ page/plugin.config.js
@@ -2,7 +2,7 @@ export default {
   name: 'frontend-customizer',
   displayName: 'Frontend Customizer - MOS',
   description: 'Advanced frontend customizer with themes, branding & layout options for MOS',
-  version: '2.3.0',
+  version: '2.3.1',
   icon: 'mdi-palette-advanced',
   author: 'RiDDiX',
   homepage: 'https://github.com/RiDDiX/mos-frontend-customizer',
